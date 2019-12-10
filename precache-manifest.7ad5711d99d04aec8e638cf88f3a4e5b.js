e21b3f2dbe0ccb8363d2f1aab5f02c3923bca23a
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2019-12-10 10:54:16 +0200

    Updates

---
+++ precache-manifest.7ad5711d99d04aec8e638cf88f3a4e5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33059e284fc06e07275fafa88c9a5379",
+    "url": "/chat-app/./index.html"
+  },
+  {
+    "revision": "1dee2274c129c26bb9c2",
+    "url": "/chat-app/./static/css/main.8c6ecf8f.chunk.css"
+  },
+  {
+    "revision": "9127aef977abf4fbc20c",
+    "url": "/chat-app/./static/js/2.66695ea2.chunk.js"
+  },
+  {
+    "revision": "1dee2274c129c26bb9c2",
+    "url": "/chat-app/./static/js/main.201c85d4.chunk.js"
+  },
+  {
+    "revision": "448ddfead92e53412560",
+    "url": "/chat-app/./static/js/runtime-main.f28abbf0.js"
+  }
+]);
